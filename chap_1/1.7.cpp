98f6cb5816ed8fab75374d34d56523eae4412a87
Wesley Andrade <wafermonster@yahoo.com>
2017-12-31 03:26:46 -0600

    added rotate counterclockwise

--- chap_1/1.7.cpp
+++ chap_1/1.7.cpp
@@ -8,7 +8,7 @@ using namespace std;
 // Given an image represented by an NxN matrix, where each pixel in the image is 4
 // bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
-void rotate(vector<vector<int>> vect) {
+void rotateCounterClockwise(vector<vector<int>> vect) {
 	
 	for (int i = vect.size()-1; i > -1; i--) {
 		for (int j = 0; j < vect[i].size(); j++) {
@@ -20,12 +20,26 @@ void rotate(vector<vector<int>> vect) {
 }
 
 
+void rotateClockwise(vector<vector<int>> vect) {
+
+	for (int i = 0; i < vect[0].size(); i++) {
+		for (int j = vect[0].size() - 1; j > -1; j--) {
+			cout << vect[j][i] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+
 int main() {
 	vector<vector<int>> vect{ { 3, 5, 1 },
-							  { 4, 8, 6 },
-							  { 7, 2, 9 } };
+				  { 4, 8, 6 },
+				  { 7, 2, 9 } };
 		
-	rotate(vect);
+	rotateCounterClockwise(vect);
+	rotateClockwise(vect);
+	
 	
 	system("pause");
 	return 0;
